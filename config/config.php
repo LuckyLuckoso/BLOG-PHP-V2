e499b5d556bb409fad54dd431b599aa222db4a59
LuckyLuckoso <70530022+LuckyLuckoso@users.noreply.github.com>
2021-04-28 21:52:39 -0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+	$_blog_title = 'Meu Blog';
+	$_blog_autor = 'By João Pedro';
+	$_blog_search = 'Pesquisa';
+	$_blog_destaque = 'Destaques';
+	$_blog_post = 'Postagens';
+	$_blog_read = 'Leia mais';
+	$_blog_address = 'Endereço';
+	$_blog_sponsor = 'Patrocinado';
+?>
---
+++ index.php
@@ -0,0 +1,76 @@
+<!-- CONFIG DO BLOG -->
+<?php
+require_once ('config/config.php');
+?>		
+
+<!-- HEAD -->
+
+<?php
+include 'inc/head.php';
+?>
+
+<body class="is-preload">
+	<!-- Wrapper -->
+	<?php
+	include 'inc/wrapper.php';
+	?>
+
+	<!-- Main -->
+	<?php
+	include 'inc/main.php';
+	?>
+
+	<!-- Header -->
+	<?php
+	include 'inc/header.php';
+	?>
+
+	<!-- Banner -->
+	<?php
+	include 'inc/banner.Php';
+	?>	
+
+	<!-- Section -->
+	<?php
+	include 'inc/include1.php';
+	?>
+
+
+
+	<!-- Sidebar -->
+	<?php
+	include 'inc/sidebar.php';
+	?>
+	<!-- Search -->
+	<?php
+	include 'inc/search.php';
+	?>
+
+	<!-- Menu -->
+	<?php
+	include 'inc/menu.php';
+	?>								
+
+	<!-- Section -->
+	<?php
+	include 'inc/sectiond.php';
+	?>
+
+	<!-- Section -->
+	<?php
+	include 'inc/sectiont.php'
+	?>
+
+	<!-- Footer -->
+	<?php
+	include 'inc/footer.php'
+	?>
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>
